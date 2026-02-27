4a8cd52723911b99b0a7edd33854f3239c0657aa
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2026-02-27 22:01:57 +0000

    Prueba de funcionamiento de form

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("form.js cargado");
     const form = document.getElementById("contact-form");
     const statusBox = document.getElementById("form-status");
 
